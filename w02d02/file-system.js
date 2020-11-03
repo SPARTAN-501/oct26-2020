7c8a8cdcd9d9b78dc89cfd3519fe0c6f2d452e26
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-11-03 11:44:20 -0800

    update w2d2 readme

--- w02d02/file-system.js
+++ w02d02/file-system.js
@@ -5,13 +5,14 @@ console.log('sync', returnValue.length);
 
 // everything's fine
 // callback(null, data);
+
 // everything's not fine
 // callback(err);
 
 fs.readFile('./index.html', { encoding: 'utf-8' }, (err, data) => {
-  // if (err) {
-  //   return console.log(err);
-  // }
+  if (err) {
+    return console.log(err);
+  }
 
   console.log('async', data.length);
 });
