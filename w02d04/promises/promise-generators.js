5bd1517a3df20639f3bfb66f906db988e9d7a6fd
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-11-05 09:19:29 -0800

    update promise generators

--- w02d04/promises/promise-generators.js
+++ w02d04/promises/promise-generators.js
@@ -1,12 +1,12 @@
 const returnPromise = (delay = 1000) => {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, delay);
+    setTimeout(() => resolve('yay! resolved!'), delay);
   });
 };
 
 const returnRejectedPromise = (delay = 1000) => {
   return new Promise((resolve, reject) => {
-    setTimeout(reject, delay);
+    setTimeout(() => reject('doh!'), delay);
   });
 };
 
@@ -14,11 +14,11 @@ const returnRandomPromise = (delay = 1000) => {
   const num = Math.floor(Math.random() * 2);
   if (num < 1) {
     return new Promise((resolve, reject) => {
-      setTimeout(resolve, delay);
+      setTimeout(() => resolve('yay! resolved!'), delay);
     });
   }
   return new Promise((resolve, reject) => {
-    setTimeout(reject, delay);
+    setTimeout(() => reject('doh!'), delay);
   });
 };
 
